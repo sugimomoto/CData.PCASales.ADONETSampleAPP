9cbbccdafe89aa728ded16eea67e4a120a0b897a
Kazuya Sugimoto <sugimomoto@gmail.com>
2019-02-20 13:52:26 +0900

    new

--- WindowsFormsApp6/Form1.Designer.cs
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -29,12 +29,130 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataSet1 = new WindowsFormsApp6.DataSet1();
+            this.masterTmsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.masterTmsTableAdapter = new WindowsFormsApp6.DataSet1TableAdapters.MasterTmsTableAdapter();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterTmsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // masterTmsBindingSource
+            // 
+            this.masterTmsBindingSource.DataMember = "MasterTms";
+            this.masterTmsBindingSource.DataSource = this.dataSet1;
+            // 
+            // masterTmsTableAdapter
+            // 
+            this.masterTmsTableAdapter.ClearBeforeFill = true;
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "得意先マスタ読み込み";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "商品マスター読み込み";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "見積伝票読み込み";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 169);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 361);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(455, 123);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(698, 122);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "実行";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 542);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterTmsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource masterTmsBindingSource;
+        private DataSet1TableAdapters.MasterTmsTableAdapter masterTmsTableAdapter;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WindowsFormsApp6/Form1.cs
+++ WindowsFormsApp6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.CData.PCASales;
 
 namespace WindowsFormsApp6
 {
@@ -15,6 +16,46 @@ namespace WindowsFormsApp6
         public Form1()
         {
             InitializeComponent();
+
+        }
+        
+        // 得意先マスタ（MasterTMS）
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sql = "SELECT TokuisakiCode, TokuisakiMei1,YubinBango,Jyusyo1,AitesakiTelNo FROM MasterTMS limit 15";
+            textBox1.Text = sql;
+        }
+
+        // 商品マスタ（MasterSMS）
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sql = "SELECT * FROM MasterSMS limit 15";
+            textBox1.Text = sql;
+        }
+
+        // 見積伝票（InputMIT）
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var sql = "SELECT * FROM InputMIT limit 15";
+            textBox1.Text = sql;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var sql = textBox1.Text;
+
+            string connectionString = "CallbackURL=XXXXXX;DefaultDataArea=XXXXXXX;InitiateOAuth=GETANDREFRESH;OAuth Client Id=XXXXXXXOAuth Client Secret=XXXXXXX;DataCenter=east02;ProductCode=Kon20;";
+            
+            using (var connection = new PCASalesConnection(connectionString))
+            {
+                var dataAdapter = new PCASalesDataAdapter(
+                sql, connection);
+
+                var table = new DataTable();
+                dataAdapter.Fill(table);
+
+                dataGridView1.DataSource = table;
+            }
         }
     }
 }
